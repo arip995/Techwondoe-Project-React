71a986f041af9a87534a580d8ca7a16aad8a0fd4
arip995 <arindampanda2002@gmail.com>
2022-08-16 08:48:18 +0530

    added button stories to the storybook

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -5,19 +5,15 @@ interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
    */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
+  type?: 'primary' | 'secondary';
   /**
    * Button contents
    */
-  label: string;
+  children: string;
+  /**
+   * Optional click handler
+   */
+  fontFamily?: 'Poppins' | 'monospace';
   /**
    * Optional click handler
    */
@@ -28,21 +24,26 @@ interface ButtonProps {
  * Primary UI component for user interaction
  */
 export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
+  type = 'primary',
+  fontFamily = 'Poppins',
+  children,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode =
+    type === 'primary'
+      ? 'storybook-button--primary'
+      : 'storybook-button--secondary';
+  const font =
+    fontFamily === 'Poppins'
+      ? 'storybook-button--Poppins'
+      : 'storybook-button--monospace';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
+      className={['storybook-button', mode, font].join(' ')}
       {...props}
     >
-      {label}
+      {children}
     </button>
   );
 };
