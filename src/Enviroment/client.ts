915d815a1a9a9efad6d28be79db795bc7f64f887
arip995 <arindampanda2002@gmail.com>
2022-08-16 12:15:36 +0530

    refactored typography

--- src/Enviroment/client.ts
+++ src/Enviroment/client.ts
@@ -1,5 +1,5 @@
-import { createClient } from "contentful";
+import { createClient } from 'contentful';
 export const Client = createClient({
-    space: 'd7h9e09vtxna',
-    accessToken: 'l1vfi-82oIjzB3HhYIEQRct2eKEhFbcBfDCgGNXt1j8'
-})
+  space: 'd7h9e09vtxna',
+  accessToken: 'l1vfi-82oIjzB3HhYIEQRct2eKEhFbcBfDCgGNXt1j8',
+});
