dd4bf4af042761b1e7fb13a5216920b6210e6f88
arip995 <arindampanda2002@gmail.com>
2022-08-16 06:47:20 +0530

    added prettier and linter config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import React from 'react';
 function App() {
-	return (
-		<div>
-			<div className="flex flex-row">
-				<div className="basis-1/4">01</div>
-				<div className="basis-1/4">02</div>
-				<div className="basis-1/2">03</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="flex flex-row">
+        <div className="basis-1/4">01</div>
+        <div className="basis-1/4">02</div>
+        <div className="basis-1/2">03</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
