ee26c19541f7d6062e592862e5b9827cf961afeb
arip995 <arindampanda2002@gmail.com>
2022-08-18 14:45:15 +0530

    updated component naming

--- src/stories/Components/Typography/Typography.stories.tsx
+++ src/stories/Components/Typography/Typography.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Typography from './Typography';
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Techwondoe/Typography',
   component: Typography,
--- src/stories/Components/WhyUs/WhyUs.stories.tsx
+++ src/stories/Components/WhyUs/WhyUs.stories.tsx
@@ -3,21 +3,17 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { WhyUs } from './WhyUs';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Techwondoe/WhyUs',
   component: WhyUs,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
 } as ComponentMeta<typeof WhyUs>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof WhyUs> = (args) => <WhyUs {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   headinglabel: 'Large Client Networks',
   headingType: 'h2',
